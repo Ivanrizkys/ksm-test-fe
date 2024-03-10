0867046fb51daf2475bae3adb6cb75a4b01e08fa
Ivanrizkys <ivnrzkys@gmail.com>
2024-03-10 14:06:34 +0700

    chore: setup redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
+import { store } from "@/global/store";
 import type { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/sonner";
+import { Provider as ReduxProvider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ReduxProvider store={store}>
       <Component {...pageProps} />
       <Toaster position="top-right" />
-    </>
+    </ReduxProvider>
   );
 }
